53a5962606449957c45b8fe9f457b80e69bb4f01
RY <e5277@outlook.com>
2026-07-21 10:47:23 +0800

    Update src/config/profileConfig.ts (via Pages CMS)

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "https://img.axio.top/uploads/2026/06/18b58238314dc336649.webp",
 
 	// 名字
-	name: "Firefly",
+	name: "额丶Y",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm 额丶Y.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -30,13 +30,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/RY-zzcn",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:ey@mail.axioy.cn",
 			showName: false,
 		},
 		{
